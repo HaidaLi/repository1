d3409b9a295faec901d6cb4bfb568f9e4ac4ab47
Haida <lhaidali@gmail.com>
2023-03-13 16:51:38 +0100

    restarr

--- Github/HelloWorld.java
+++ Github/HelloWorld.java
@@ -6,9 +6,14 @@ public class HelloWorld {
         System.out.println("HOla");
         System.out.println("AAAAAA");
         System.out.println(sumar(5,6));
+        System.out.println(restar(5,6));
+
     }
     public static int sumar(int x,int y) {
         return  x+y;
+    }
+    public static int  restar(int x, int y) {
+        return x-y;
     }
 
 }
