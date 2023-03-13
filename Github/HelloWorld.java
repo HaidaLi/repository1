6ae47f7f195d18761d24c3753256f6d980c004cd
Haida <lhaidali@gmail.com>
2023-03-13 16:32:17 +0100

    He agregado un sout

--- Github/HelloWorld.java
+++ Github/HelloWorld.java
@@ -1,7 +1,10 @@
 package Github;
 
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("HOla");
     }
 }
